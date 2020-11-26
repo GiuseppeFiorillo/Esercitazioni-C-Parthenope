b9c278c430415eb66c11d1be0f2b0f6ef7e45967
Giuseppe Fiorillo <74118927+GiuseppeFiorillo@users.noreply.github.com>
2020-11-26 13:02:14 +0100

    Create ordinamento_per_inserimento_stringhe.c

---
+++ Algoritmi di ordinamento/ordinamento_per_inserimento_stringhe.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+void ord_ins_sP(char**, size_t);
+
+int main(int argc, const char * argv[])
+{
+    char* elenco[] = {
+        "Giuseppe",
+        "Anna",
+        "Raffaele"
+    };
+    
+    ord_ins_sP(elenco, 3);
+    for(int i = 0; i < 3; ++i)
+        printf("%s\n", elenco[i]);
+    
+    return 0;
+}
+
+void ord_ins_sP(char** elenco, size_t size)
+{
+    size_t i;
+    int j;
+    char* el_da_ins;
+    for(i = 1; i < size; ++i)
+    {
+        el_da_ins = elenco[i];
+        j = i-1;
+        while(j >= 0 && strcmp(el_da_ins, elenco[j]) < 0 )
+        {
+            elenco[j+1] = elenco[j];
+            --j;
+        }
+        elenco[j+1] = el_da_ins;
+    }
+}
