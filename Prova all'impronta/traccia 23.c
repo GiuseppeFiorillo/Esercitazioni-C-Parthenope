a92fe3a3b9eb9fd96d10ec24132fb650e84c9b15
Giuseppe Fiorillo <74118927+GiuseppeFiorillo@users.noreply.github.com>
2020-12-13 16:56:45 +0100

    Create traccia 23.c

---
+++ Prova all'impronta/traccia 23.c
@@ -0,0 +1,81 @@
+/*Scrivere una function C che ha come input i dati che identificano uno studente (nome, cognome,
+  matricola) e che restituisce in output una struttura dati opportuna, che contiene i dati di
+  identificazione e il libretto universitario con al massimo 20 esami. Ogni esame è caratterizzato da
+  denominazione, cfu e voto.*/
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char nome[100];
+    char cognome[100];
+    int matricola;
+} studente;
+
+typedef struct {
+    char materia[30];
+    int cfu;
+    int voto;
+} esame;
+
+typedef struct {
+    studente alunno;
+    esame libretto[20];
+} carriera;
+
+void esercizio(char*, char*, int, carriera*, int *num_esami);
+
+int main(int argc, const char * argv[])
+{
+    char nome[100], cognome[100];
+    int matricola, num_esami;
+    carriera stud;
+    
+    printf("Inserisci il tuo nome: ");
+    fgets(nome, 100, stdin);
+    nome[strcspn(nome, "\n")] = 0;
+    
+    printf("Inserisci il tuo cognome: ");
+    fgets(cognome, 100, stdin);
+    cognome[strcspn(cognome, "\n")] = 0;
+    
+    printf("Inserisci la tua matricola: ");
+    scanf("%d", &matricola);
+    
+    esercizio(nome, cognome, matricola, &stud, &num_esami);
+    printf("\nNome: %s\tCognome: %s\tMatricola: %d\n", stud.alunno.nome, stud.alunno.cognome, stud.alunno.matricola);
+    
+    for(size_t i = 0; i < num_esami; i++)
+    {
+        printf("Voto %s di %d CFU: %d\n", stud.libretto[i].materia, stud.libretto[i].cfu, stud.libretto[i].voto);
+    }
+    
+    return 0;
+}
+
+void esercizio(char *nome, char *cognome, int matricola, carriera *stud, int *num_esami)
+{
+    studente dati;
+    size_t i = 0;
+    strcpy(dati.nome, nome);
+    strcpy(dati.cognome, cognome);
+    dati.matricola = matricola;
+    
+    stud->alunno = dati;
+    
+    printf("Quanti esami hai dato?\n");
+    scanf("%d%*c", num_esami);
+    for(i = 0; i < *num_esami; i++)
+    {
+        printf("Inserire il nome della materia del %zu esame: ", i+1);
+        fgets(stud->libretto[i].materia, 30, stdin);
+        stud->libretto[i].materia[strcspn(stud->libretto[i].materia, "\n")] = 0;
+        
+        printf("Inserire il voto di %s: ", stud->libretto[i].materia);
+        scanf("%d", &stud->libretto[i].voto);
+        
+        printf("Inserire il numero di CFU: ");
+        scanf("%d%*c", &stud->libretto[i].cfu);
+        putchar('\n');
+    }
+}
