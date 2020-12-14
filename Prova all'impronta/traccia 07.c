cc7191fe0de23e820a74a658abfcdeac0b3ab412
Giuseppe Fiorillo <74118927+GiuseppeFiorillo@users.noreply.github.com>
2020-12-14 11:23:21 +0100

    Update traccia 07.c

--- Prova all'impronta/traccia 07.c
+++ Prova all'impronta/traccia 07.c
@@ -5,7 +5,7 @@ che due dati struct sono uguali se sono uguali tutti i loro campi.*/
 #include <stdio.h>
 
 typedef struct {
-    char* nome;
+    char *nome;
     int codice;
     double prezzo;
 } prodotto;
@@ -13,7 +13,8 @@ typedef struct {
 int uguale(prodotto[], size_t, prodotto[], size_t);
 int uguaglianza(prodotto, prodotto);
 
-int main() {
+int main() 
+{
     prodotto lista1[2] = {
             {"Nintendo DS", 100, 250},
             {"Sony PSP", 101, 300}
@@ -29,11 +30,13 @@ int main() {
     return 0;
 }
 
-int uguale(prodotto lista1[], size_t n, prodotto lista2[], size_t m) {
+int uguale(prodotto lista1[], size_t n, prodotto lista2[], size_t m) 
+{
     size_t i;
     if(n != m)
         return 0;
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) 
+    {
         if(!(uguaglianza(lista1[i], lista2[i])))
             return 0;
     }
